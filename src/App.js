681860f89f73c574da975203d5241912dcd3e7d1
joedanks <joedanks@gmail.com>
2018-08-15 22:26:42 -0500

    green

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.test.js</code> in TDD fashion.
         </p>
-        <input type='file' accept='image/png, image/jpeg' onChange={()=>{}}/>
+        <input type='file' accept='image/*' onChange={()=>{}}/>
         <img className='preview'/>
       </div>
     );
